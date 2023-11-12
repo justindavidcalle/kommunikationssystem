33bc7eec7e1e1fc64b68933908f2e63ccd60edf0
justindavidcalle <justincalle@outlook.de>
2023-11-12 12:22:42 +0100

    userrouter completed

--- backend/routes/userroute.js
+++ backend/routes/userroute.js
@@ -14,7 +14,9 @@ router.get('/', async (req, res) => {
 })
 
 //GET ID
-
+router.get('/:id', getUsers , (req, res)=> {
+    res.send(res.user)
+})
 
 //POST ONE
 router.post('/', async (req,res) => {
@@ -30,5 +32,47 @@ router.post('/', async (req,res) => {
         res.status(400).json({message: err.message})
     }
 })
+
+//UPDATE USER BY ID
+router.patch('/:id', getUsers, async (req, res) => {
+    if (req.body.username != null) {
+        res.user.username = req.body.username;
+    }
+    if (req.body.eMail != null) {
+        res.user.eMail = req.body.eMail;
+    }
+    try {
+        const updatedUser = await res.user.save();
+        res.json(updatedUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+
+// DELETE USER BY ID
+router.delete('/:id', getUsers, async (req, res)=>{
+    try{
+        await User.deleteOne({ _id: req.params.id})
+        res.json({message: "Deleted User"})
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+async function getUsers(req, res, next){
+    let user
+    try{
+        user= await User.findById(req.params.id)
+        if(user == null){
+            return res.status(404).json({message:'Cannot find User'})
+        }
+
+    } catch(err){
+        return res.status(500).json({message: err.message})
+    }
+    res.user = user
+    next()
+}
 
 module.exports = router
